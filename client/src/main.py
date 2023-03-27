5c5ae0f2ebd20f0376140ef3a269ed80e34c4cbc
Magnus K <magnusku@gmx.de>
2023-03-27 23:20:36 +0200

    added tilemap rendering and a test map

--- client/src/main.py
+++ client/src/main.py
@@ -4,17 +4,37 @@ from pygame.locals import *
 from tilemap import *
 
 PATHTOTILEIMAGES = "TombstoneTunnels\\client\\assets\\tilemap\\tiles"
+PATHTOTESTMAP = "TombstoneTunnels\\client\\assets\\tilemap\\maps\\testmap.csv"
 
 pygame.init()
 
-pygame.display.set_mode((500,600))
+root = pygame.display.set_mode((1400,800))
+mainSurf = pygame.Surface((280,160))
 
 running = True
 
-tileMap = TileMap(16, PATHTOTILEIMAGES)
-print(tileMap.tileTypeManager.tileTypes)
-
+tileMap = TileMap(16, PATHTOTILEIMAGES, PATHTOTESTMAP, pygame.Vector2(0,0))
+ofx=0
+ofy=0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN and event.key == K_d:
+            ofx+=10
+        elif event.type == pygame.KEYDOWN and event.key == K_a:
+            ofx-=10
+        
+        if event.type == pygame.KEYDOWN and event.key == K_w:
+            ofy-=10
+        elif event.type == pygame.KEYDOWN and event.key == K_s:
+            ofy+=10
+    
+    tileMap.render(mainSurf, pygame.Vector2(ofx,ofy))
+
+    root.blit(pygame.transform.scale(mainSurf, (1400,800)), (0,0))
+    
+    pygame.display.flip()
+    
+    mainSurf.fill((45,45,40))
--- client/src/tilemap.py
+++ client/src/tilemap.py
@@ -5,8 +5,11 @@ import pygame, os
 
 # data type to be stored in the tileTypeManager
 class TileType:
-    def __init__(self, imgPath, solid:bool=False):
-        self.image = pygame.image.load(imgPath).convert_alpha()
+    def __init__(self, imgPath:str, solid:bool=False):
+        try:
+            self.image = pygame.image.load(imgPath).convert_alpha()
+        except:
+            print(f"failed to load tile from path: {imgPath}")
         self.solid = solid
 
 # contains all possible TileTypes and their names
@@ -14,20 +17,43 @@ class TileTypeManager:
     def __init__(self):
         self.tileTypes = {}
 
-    def loadImages(self, path):
+    def loadImages(self, path:str):
         for filename in os.listdir(path):
             name = filename[:filename.find(".")]
             self.tileTypes[name] = TileType(path+"\\"+filename)
 
+
 class TileMap:
-    def __init__(self, size, pathToTileImgs):
+    def __init__(self, tileSize:int, pathToTileImgs:str, pathToMapFile:str, pos:pygame.Vector2=pygame.Vector2(0,0)):
+        self.tileSize = tileSize
         self.tileTypeManager = TileTypeManager()
         self.tileTypeManager.loadImages(pathToTileImgs)
-        self.tileMap = [[] for x in range(size)] # list of tile names
+        self.position = pos
 
-    def loadMapFile(self, path):
-        with open(path, "r") as fp:
-            data = fp.readlines()
+        self.tileMap = [] # list of tile names
+        self.loadMapFile(pathToMapFile)
 
-        for line in data:
-            data[line] = data[line].replace(" ", "").split(",")
+    def loadMapFile(self, path:str):
+        try:
+            with open(path, "r") as fp:
+                data = fp.readlines()
+        except:
+            print(f"Failed to load mapfile at {path}")
+        for index, line in enumerate(data):
+            data[index] = line.replace(" ", "").replace("\n", "").split(",")
+
+        self.tileMap = data
+
+    def render(self, surface:pygame.Surface, offset:pygame.Vector2): # unoptimised rendering, assuming tilemap is relatively small
+        for y, row in enumerate(self.tileMap):
+            for x, tilename in enumerate(row):
+                surface.blit(
+                    self.tileTypeManager.tileTypes[tilename].image,
+                    (
+                        self.position.x+(x*self.tileSize)+offset.x, 
+                        self.position.y+(y*self.tileSize)+offset.y
+                    )
+                )
+
+        
+
